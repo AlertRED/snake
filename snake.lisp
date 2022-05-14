f138e7a692e5c15135f201f394b6e5bb69184aaf
Aleksandr <drammtv@gmail.com>
2022-05-15 01:20:54 +0300

    Just snake in the field

---
+++ snake.lisp
@@ -0,0 +1,134 @@
+(load "/home/merov/quicklisp/setup.lisp")
+(ql:quickload "bt-semaphore")
+(ql:quickload "trivial-raw-io")
+(defclass point ()
+    ((x :initarg :x) 
+    (y :initarg :y)))
+
+(defclass game ()
+    ((snake
+    :initarg :snake
+    :initform (list (make-instance 'point :x 2 :y 4)
+                    (make-instance 'point :x 2 :y 3)
+                    (make-instance 'point :x 2 :y 2)
+                ))
+    (direction
+    :initarg :direction
+    :initform 0)
+    (is_pause
+    :initarg :is_pause
+    :initform Nil
+    )
+    ))
+
+(defmethod growup ((game1 game))
+    (setf (slot-value game1 'snake) 
+        (append (slot-value game1 'snake) 
+                (list (make-instance 'point :x 3 :y 3))
+        )
+    )
+    T
+    )
+
+(defun cls()
+  (format t "~A[H~@*~A[J" #\escape)
+  (format t "~A[H~@*~A[J" #\escape))
+
+(defmethod move ((game1 game))
+    (let ((x (- (length (slot-value game1 'snake)) 1)))
+        (loop repeat (- (length (slot-value game1 'snake)) 1) do
+            
+            (setf (slot-value (nth x (slot-value game1 'snake)) 'x)
+                (slot-value (nth (- x 1) (slot-value game1 'snake)) 'x))
+            
+            (setf (slot-value (nth x (slot-value game1 'snake)) 'y)
+                (slot-value (nth (- x 1) (slot-value game1 'snake)) 'y))
+            (decf x)
+        )
+    )
+
+    (cond 
+        ((eql (slot-value game1 'direction) 0) (incf (slot-value (first (slot-value game1 'snake)) 'y))) 
+        ((eql (slot-value game1 'direction) 1) (incf (slot-value (first (slot-value game1 'snake)) 'x))) 
+        ((eql (slot-value game1 'direction) 2) (decf (slot-value (first (slot-value game1 'snake)) 'y))) 
+        ((eql (slot-value game1 'direction) 3) (decf (slot-value (first (slot-value game1 'snake)) 'x))) 
+    )
+)
+
+(defmethod run ((game1 game))
+    (loop 
+        (if (eql (slot-value game1 'is_pause) nil)
+            (progn
+                (cls)
+                (move game1)
+                ; (growup game1)
+                (draw-field game1)
+                (sleep 0.5)
+            )
+        )
+    ))
+
+(defmethod draw-field ((game1 game))
+    (defvar width 20)
+    (defvar height 10)
+    (defvar y 0)
+    (defvar x 0)
+    (setq y 0)
+
+    (format t "Score: 0")
+    (terpri)(format t "~v@{~A~:*~}" (+ width 2) "-")
+    (loop repeat height do 
+        (setq x 0)
+        (incf y)
+        (terpri)
+        (format t "~a" "|")
+        (loop repeat width do 
+            (incf x)
+            (let ((found nil))
+                (loop for point in (slot-value *game* 'snake) do
+                    (if (and
+                            (= (slot-value point 'x) x)
+                            (= (slot-value point 'y) y)
+                        )
+                        (progn (setf found T) (return))
+                    )   
+                )
+                (if found
+                    (format t "-")
+                    (format t "+"))
+            )
+            
+        )
+        (format t "~a" "|")
+    )
+    (terpri)(format t "~v@{~A~:*~}" (+ width 2) "-")
+    (terpri)(format t "~v@{~A~:*~}" (+ width 2) "-"))
+    
+(defparameter *game*
+    (make-instance 'game))
+
+(defun key-down ()
+    (bt:make-thread
+    (lambda ()
+        (let ((x nil))
+            (loop
+                (setf x (trivial-raw-io:read-char))
+                (cond 
+                    ((and (eql x #\A) (/= (slot-value *game* 'direction) 0)) 
+                        (setf (slot-value *game* 'direction) 2))
+                    ((and (eql x #\B) (/= (slot-value *game* 'direction) 2))
+                        (setf (slot-value *game* 'direction) 0))
+                    ((and (eql x #\C) (/= (slot-value *game* 'direction) 3))
+                         (setf (slot-value *game* 'direction) 1))
+                    ((and (eql x #\D) (/= (slot-value *game* 'direction) 1)) 
+                        (setf (slot-value *game* 'direction) 3))
+                    ((eql x #\Space) 
+                        (setf (slot-value *game* 'is_pause) (not (slot-value *game* 'is_pause))))
+                )
+            )
+        )
+        ))
+    T)
+
+(key-down)
+(run *game*)
