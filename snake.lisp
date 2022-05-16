cef0119d72c7b3deb8bade09d23add3546fdaef4
Aleksandr <drammtv@gmail.com>
2022-05-16 23:53:36 +0300

    added food and growup when snake eating

--- snake.lisp
+++ snake.lisp
@@ -19,20 +19,55 @@
     :initarg :is_pause
     :initform Nil
     )
+    (food
+    :initarg :food
+    :initform (list (make-instance 'point :x 2 :y 7) ;(make-random-state t)
+                ))
     ))
 
+(defmethod new-food((game1 game))
+    (push (make-instance 'point :x (random 20) :y (random 10)) (slot-value game1 'food))
+)
+
 (defmethod growup ((game1 game))
-    (setf (slot-value game1 'snake) 
-        (append (slot-value game1 'snake) 
-                (list (make-instance 'point :x 3 :y 3))
+    (let ((x (slot-value (first (slot-value game1 'snake)) 'x)) (y (slot-value (first (slot-value game1 'snake)) 'y)))
+        (cond 
+            ((= (slot-value game1 'direction) 0) (push (make-instance 'point :x x :y (+ y 1)) (slot-value game1 'snake))) 
+            ((= (slot-value game1 'direction) 1) (push (make-instance 'point :x (+ x 1) :y y) (slot-value game1 'snake))) 
+            ((= (slot-value game1 'direction) 2) (push (make-instance 'point :x x :y (- y 1)) (slot-value game1 'snake))) 
+            ((= (slot-value game1 'direction) 3) (push (make-instance 'point :x (- x 1) :y y) (slot-value game1 'snake))) 
         )
     )
-    T
-    )
+)
 
 (defun cls()
   (format t "~A[H~@*~A[J" #\escape)
   (format t "~A[H~@*~A[J" #\escape))
+
+(defun remove-food (food game1)
+    (let ((new-list))
+        (loop for point in (slot-value game1 'food) do
+            (if (or (/= (slot-value point 'x)(slot-value food 'x)) (/= (slot-value point 'y)(slot-value food 'y)))
+                (setf new-list (append (slot-value game1 'food) 
+                    (list (make-instance 'point :x 3 :y 3)))
+                )
+            ) 
+        )
+        (setf (slot-value game1 'food) new-list)
+    ))
+
+(defmethod eat ((game1 game))
+    (let ((head (first (slot-value game1 'snake))))
+        (loop for point in (slot-value *game* 'food) do
+            (if (and
+                    (= (slot-value point 'x) (slot-value head 'x))
+                    (= (slot-value point 'y) (slot-value head 'y))
+                )
+                (progn (remove-food point game1) (return T))
+            )
+        )
+    )
+)
 
 (defmethod move ((game1 game))
     (let ((x (- (length (slot-value game1 'snake)) 1)))
@@ -54,18 +89,24 @@
         ((eql (slot-value game1 'direction) 3) (decf (slot-value (first (slot-value game1 'snake)) 'x))) 
     )
 )
-
+  
 (defmethod run ((game1 game))
-    (loop 
+    (loop do
         (if (eql (slot-value game1 'is_pause) nil)
             (progn
                 (cls)
-                (move game1)
-                ; (growup game1)
+                (if (eat game1)
+                    (progn
+                        (growup game1)
+                        (new-food game1)
+                    )
+                    (move game1)
+                )
                 (draw-field game1)
-                (sleep 0.5)
+                (setf point-food nil)
             )
         )
+        (sleep 0.5)
     ))
 
 (defmethod draw-field ((game1 game))
@@ -84,20 +125,29 @@
         (format t "~a" "|")
         (loop repeat width do 
             (incf x)
-            (let ((found nil))
+            (let ((is_snake nil)(is_food nil))
                 (loop for point in (slot-value *game* 'snake) do
                     (if (and
                             (= (slot-value point 'x) x)
                             (= (slot-value point 'y) y)
                         )
-                        (progn (setf found T) (return))
+                        (progn (setf is_snake T) (return))
                     )   
                 )
-                (if found
-                    (format t "-")
-                    (format t "+"))
+                (loop for point in (slot-value *game* 'food) do
+                    (if (and
+                            (= (slot-value point 'x) x)
+                            (= (slot-value point 'y) y)
+                        )
+                        (progn (setf is_food T) (return))
+                    )
+                )
+                (cond
+                    (is_snake (format t "-"))
+                    (is_food (format t "@"))
+                    (T (format t "+"))
+                )
             )
-            
         )
         (format t "~a" "|")
     )
@@ -132,3 +182,12 @@
 
 (key-down)
 (run *game*)
+
+; (let ((f '((make-instance 'point :x 2 :y 4)
+;                     (make-instance 'point :x 2 :y 3)
+;                     (make-instance 'point :x 2 :y 2)
+;                 )))
+
+;     (remove-nth 0 '(2 3))             
+;     (print f)
+; )
